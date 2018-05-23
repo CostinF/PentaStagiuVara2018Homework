56abac99e70d4b08155ecf3d8ea15e113560fdd5
Costin <costin.faraian@gmail.com>
2018-05-23 12:38:03 +0300

    initial commit

---
+++ Module01Week02/Homework01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework01
+{
+    class Program
+    {
+
+        public static string GetBirthDate(int yearOfBirth, int monthOfBirth, int dayOfBirth)
+        {
+
+            string format = "dd/MM/yyyy";
+            DateTime birthDate = new DateTime(yearOfBirth, monthOfBirth, dayOfBirth);
+            return birthDate.ToString(format);
+
+        }
+
+        public static int GetAge(int yearOfBirth)
+        {
+
+            var today = DateTime.Today;
+            var age = today.Year - yearOfBirth;
+            return age;
+
+        }
+
+        public enum Gender
+        {
+
+            Male,
+            Female
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input year:");
+            int yearOfBirth = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input month:");
+            int monthOfBirth = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input day:");
+            int dayOfBirth = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your birthdate: {GetBirthDate(yearOfBirth, monthOfBirth, dayOfBirth)} and your age: { GetAge(yearOfBirth)}");
+
+            Console.WriteLine("Input gender: (M/F)");
+            char gender = Convert.ToChar(Console.ReadLine());
+
+            int? inputGender;
+
+            if (gender == 'M')
+            {
+
+                Gender selectedGender = Gender.Male;
+                inputGender = (int)selectedGender;
+
+            }
+
+            else if (gender == 'F')
+            {
+
+                Gender selectedGender = Gender.Female;
+                inputGender = (int)selectedGender;
+            }
+
+            else
+
+                inputGender = null;
+
+            switch (inputGender)
+            {
+
+                case 0:
+                    if (GetAge(yearOfBirth) >= 65)
+
+                        Console.WriteLine("Retired.");
+
+                    else
+
+                        Console.WriteLine($"{65 - GetAge(yearOfBirth)} years left until retirement.");
+
+                    break;
+
+                case 1:
+                    if (GetAge(yearOfBirth) >= 63)
+
+                        Console.WriteLine("Retired.");
+
+                    else
+
+                        Console.WriteLine($"{63 - GetAge(yearOfBirth)} years left until retirement.");
+
+                    break;
+
+                case null:
+                    Console.WriteLine("Wrong input format: M/F.");
+                    break;
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Module01Week02/Homework02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Hello, World!";
+            Console.WriteLine(text);
+
+            Console.WriteLine("Press any key to start.");
+            Console.ReadKey();
+
+            //upper
+            Console.WriteLine($"1. Uppercase: {text.ToUpper()}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //lower
+            Console.WriteLine($"2. Lowercase: {text.ToLower()}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //length
+            Console.WriteLine($"3. Length: {text.Length}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //split
+            Console.WriteLine("4. Split on space:");
+            string[] words = text.Split(' ');
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //replace 
+            Console.WriteLine($"5. Replace character l with x: {text.Replace("l", "x")}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //remove last character
+            Console.WriteLine($"6. Remove last character: {text.Remove(text.Length - 1)}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            //reverse
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            Console.Write("6. Reverse string:");
+            Console.WriteLine(array);
+
+            Console.WriteLine("Press any key to exit. :)");
+            Console.ReadKey();
+
+        }
+    }
+}
